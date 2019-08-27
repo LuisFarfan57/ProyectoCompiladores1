8c8ab258768f2afa7ffdd673d710fe112a70c5f8
Luis Farfán <luise.prado99@hotmail.com>
2019-08-27 17:39:29 -0600

    Proyecto

---
+++ src/compiladoresf1/Tokens.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compiladoresf1;
+
+/**
+ *
+ * @author luise
+ */
+public enum Tokens {
+    Reservadas,
+    Igual,
+    Suma,
+    Resta,
+    Multiplicacion,
+    Division,
+    ClausulaKleant,
+    Porcentaje,
+    MenorQue,
+    MayorQue,
+    MenorIgual,
+    MayorIgual,
+    IgualA,
+    DiferenteDe,
+    And,
+    Or,
+    SignoAdmiracionInverso,
+    PuntoComa,
+    Coma,
+    Punto,
+    AbrirCorchete,
+    CerrarCorchete,
+    AbrirParentesis,
+    CerrarParentesis,
+    AbrirLlave,
+    CerrarLlave,
+    DobleCorchete,
+    DobleParentesis,
+    DobleLlave,
+    Arroba,
+    Numeral,
+    DobleNumeral,
+    Identificador,
+    Bit,
+    Numero,
+    Cadena,
+    Float,
+    ERROR,
+    ErrorNuevaLineaCadena,
+    ErrorApostrofeCadena,
+    ErrorCadenaLarga,
+    ErrorComentarioMulti,
+    ErrorFloatPunto,
+    ErrorFloatE,
+    ErrorFloatMas,
+    ErrorComentarioAnidado,
+    ErrorCadenaIncompleta
+}
